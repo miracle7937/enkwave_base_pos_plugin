4d4c87276c39c60052b4028d5d5c02b6506fe720
ebuka.miracle <DF_Ebuka.Miracle@zenithbank.com>
2023-05-25 19:03:17 +0100

    update sherd file

--- src/main/java/com/Enkpay/alltransctionsPOS/transaction/PosTransactions.java
+++ src/main/java/com/Enkpay/alltransctionsPOS/transaction/PosTransactions.java
@@ -48,50 +48,39 @@ public class PosTransactions {
 
 
     public  void  processTransaction(SDKTransactionResult sdkTransactionResult){
-        selectTransaction(hostConfig,cardData,requestData,new TransactionResult() {
+        selectTransaction(hostConfig,cardData,requestData, (transactionResponse, requestData) -> {
 
-            @Override
-            public void onSuccess(TransactionResponse transactionResponse, TransactionRequestData requestData) {
+            if( transactionResponse.responseCode=="00" && transactionResponse.responseCode=="11"){
 
-                if( transactionResponse.responseCode=="00" && transactionResponse.responseCode=="11"){
+                FundWalletRequestData fundWalletRequestData = new FundWalletRequestData(PosTransactions.this.cardData, requestData,hostConfig );
+                new RetrofitBuilder().isFundUserWallet("https://jsonplaceholder.typicode.com").fundCustomerWallet(fundWalletRequestData).enqueue(new Callback<FundWalletResponseData>() {
+                    @Override
+                    public void onResponse(Call<FundWalletResponseData> call, Response<FundWalletResponseData> response) {
+                        if((response.code() != 200 || response.code() != 201) &&( transactionResponse.responseCode=="00" || transactionResponse.responseCode=="11") ){
 
-                    FundWalletRequestData fundWalletRequestData = new FundWalletRequestData(PosTransactions.this.cardData, requestData,hostConfig );
-                    new RetrofitBuilder().isFundUserWallet("https://jsonplaceholder.typicode.com").fundCustomerWallet(fundWalletRequestData).enqueue(new Callback<FundWalletResponseData>() {
-                        @Override
-                        public void onResponse(Call<FundWalletResponseData> call, Response<FundWalletResponseData> response) {
-                            if((response.code() != 200 || response.code() != 201) &&( transactionResponse.responseCode=="00" || transactionResponse.responseCode=="11") ){
+                            TransactionResponse transactionResponse= rollBack(hostConfig, cardData, requestData);
 
-                                TransactionResponse transactionResponse= rollBack(hostConfig, cardData, requestData);
-
-                                sdkTransactionResult.onSuccess(transactionResponse, requestData);
-
-                            }
+                            sdkTransactionResult.onSuccess(transactionResponse, requestData);
 
                         }
 
-                        @Override
-                        public void onFailure(Call<FundWalletResponseData> call, Throwable throwable) {
+                    }
 
-                            sdkTransactionResult.onError(throwable.getMessage(), requestData);
+                    @Override
+                    public void onFailure(Call<FundWalletResponseData> call, Throwable throwable) {
 
-                            //roll back
-                        }
-                    });
-                }else {
-                    sdkTransactionResult.onSuccess(transactionResponse, requestData);
+                        sdkTransactionResult.onError(throwable.getMessage(), requestData);
 
-                }
-
-                // fundwallet if successfull
-
+                        //roll back
+                    }
+                });
+            }else {
+                sdkTransactionResult.onSuccess(transactionResponse, requestData);
 
             }
 
-            @Override
-            public void onError(String message, TransactionRequestData requestData) {
 
-                // transaction fails;
-            }
+
         });
     }
 
@@ -106,13 +95,11 @@ public class PosTransactions {
             //fetch new keys
             new DownloadNibsKeys().download(null);
         }
-         TransactionResponse response=   new ProcessTransaction(hostConfig).process( cardData, requestData);
-        if(response.responseCode == "00" || response.responseCode == "11"){
-            transactionResult.onSuccess(response, requestData);
-        }else{
-            transactionResult.onError("Error occurred", requestData);
 
-        }
+         TransactionResponse response=   new ProcessTransaction(hostConfig).process( cardData, requestData);
+
+
+            transactionResult.onTransactionCompleted(response, requestData);
 
         }else{
             new ISWProcessPayment().ProcessPayment(cardData,requestData, transactionResult);
@@ -141,8 +128,7 @@ the TransactionRequestData request data is for roll backs
 */
     public interface TransactionResult{
 
-        public void onSuccess(TransactionResponse response, TransactionRequestData requestData);
-        public void onError(String message, TransactionRequestData requestData);
+        public void onTransactionCompleted(TransactionResponse response, TransactionRequestData requestData);
 
     }
 
