da4c178e24493f3db3e4cdefd6209b3ce4580acc
ebuka.miracle <DF_Ebuka.Miracle@zenithbank.com>
2023-05-30 10:49:23 +0100

    Shareprefrence

--- src/main/java/services/KeyValuePairStorage.java
+++ src/main/java/services/KeyValuePairStorage.java
@@ -1,12 +1,18 @@
 package services;
 
 import Var.Debug;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+
 
 import java.util.Map;
 import java.util.prefs.Preferences;
 
 public class KeyValuePairStorage {
     Preferences prefs;
+    Configuration config;
+    FileBasedConfigurationBuilder builder;
 
     private static KeyValuePairStorage instance;
     public  static KeyValuePairStorage getInstance() {
@@ -18,24 +24,43 @@ public class KeyValuePairStorage {
 
     public KeyValuePairStorage() {
 
-
-        this.prefs = Preferences.systemRoot();
+        Parameters params = new Parameters();
+        builder  =
+                new FileBasedConfigurationBuilder(KeyValuePairStorage.class)
+                        .configure(params.fileBased().setFileName("preferences.properties"));
+      try {
+          config= (Configuration) builder.getConfiguration();
+      }catch (Exception e){
+          System.out.println(e.getLocalizedMessage());
+      }
     }
 
     public void put(String key, String value) {
-        this.prefs .put(key, value);
+//        this.prefs .put(key, value);
+        config.setProperty(key, value);
+       try {
+           builder.save();
+       }catch (Exception e){
+           System.out.println(e.getLocalizedMessage());
+       }
 
     }
     public void putLong(String key, Long value) {
-        this.prefs.putLong(key,value);
-
+//        this.prefs.putLong(key,value);
+        config.setProperty(key, value);
+        try {
+            builder.save();
+        }catch (Exception e){
+            System.out.println(e.getLocalizedMessage());
+        }
     }
 
     public String get(String key) {
-        return this.prefs.get(key, "");
+            return   config.getString("myKey");
+//        return this.prefs.get(key, "");
     }
     public Long getLong(String key) {
-        return this.prefs.getLong(key,0);
+        return   config.getLong(key);
     }
 
     public void remove(String key) {
