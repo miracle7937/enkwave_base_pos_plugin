a37569169ee3fc5a69d93323660c64fd9fd64c5c
ebuka.miracle <DF_Ebuka.Miracle@zenithbank.com>
2023-08-13 16:29:25 +0100

    fail fundresponse

--- src/main/java/com/Enkpay/alltransctionsPOS/transaction/PosTransactions.java
+++ src/main/java/com/Enkpay/alltransctionsPOS/transaction/PosTransactions.java
@@ -66,8 +66,12 @@ public class PosTransactions {
             if( transactionResponse.refresh ==false){
                 FundWalletRequestData fundWalletRequestDataWithResponse =
                         new FundWalletRequestData(PosTransactions.this.cardData, requestData,hostConfig,  transactionResponse );
+
+
                 Response<FundWalletResponseData>     fundCustomerWallet =
                             new RetrofitBuilder().isFundUserWallet().fundCustomerWallet(fundWalletRequestDataWithResponse).execute();
+
+
                     if (fundCustomerWallet.body().isStatus() == true &&
                             (transactionResponse.responseCode.equals("00")
                             || transactionResponse.responseCode.equals("11"))) {
--- src/main/java/generalModel/FundWalletResponseData.java
+++ src/main/java/generalModel/FundWalletResponseData.java
@@ -1,9 +1,26 @@
 package generalModel;
 
 import lombok.Data;
+import lombok.ToString;
 
-@Data
+@ToString
 public class FundWalletResponseData {
    public boolean status;
    public String message;
+
+   public boolean isStatus() {
+      return status;
+   }
+
+   public void setStatus(boolean status) {
+      this.status = status;
+   }
+
+   public String getMessage() {
+      return message;
+   }
+
+   public void setMessage(String message) {
+      this.message = message;
+   }
 }
